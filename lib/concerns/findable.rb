584b8102779d64aa064810a202ec616039e85ea5
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-11-15 02:43:59 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,6 @@
-module Findable 
-  
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+module Findable
 
-end 
+  def find_by_name(name)
+    self.all.detect{|a| a.name == name}
+  end
+end
