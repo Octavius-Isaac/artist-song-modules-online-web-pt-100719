ca55a80e4b9f0f8f00392c0f162b1cd8b4bd5d03
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-11-15 02:39:44 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -10,8 +10,9 @@ module Memorable
     end
   end 
 
-module InstanceMethods
-  def initialize 
-    
+  module InstanceMethods
+    def initialize 
+      self.class.all << self 
+    end 
   end 
 end 
